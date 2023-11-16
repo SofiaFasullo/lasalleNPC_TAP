31dcea8c3ed3e768cc03e2ed436ae43788c727ff
Sofia <sfasull1@binghamton.edu>
2023-11-16 01:34:28 -0500

    site looks nice
    just need to display data no

--- js/filters.js
+++ js/filters.js
@@ -26,6 +26,8 @@ function initializeFilters (nonprofits, eventBus){
       filterUses();
     } else if (checkboxTypes.contains('overall-score-checkbox')){
       filterOverallScore();
+    } else if (checkboxTypes.contains('budget-checkbox')){
+      filterBudget();
     }
   }
 
@@ -63,6 +65,55 @@ function initializeFilters (nonprofits, eventBus){
           for (const nonprofit of nonprofits.features){
             var overallScore = nonprofit.properties['Overall.Score'];
             if (overallScore!==null && (overallScore==cb.value || overallScore==cb.value+0.5)){
+              filteredData.push(nonprofit);
+            }
+          }
+        }
+      }
+    }
+    const newEvent = new CustomEvent("filtered",
+    {
+      detail: filteredData
+    })
+    eventBus.dispatchEvent(newEvent)
+  }
+
+  function filterBudget(){
+    const filteredData = []
+    for (const cb of allCheckboxes){
+      if (cb.checked){
+        if (cb.value == 'null/unknown'){
+          for (const nonprofit of nonprofits.features){
+            var budget = nonprofit.properties['Annual_Operating_Budget'];
+            if (budget==null){
+              filteredData.push(nonprofit);
+            }
+          }
+        } else if (cb.value == '$8M - $850K'){
+          for (const nonprofit of nonprofits.features){
+            var budget = nonprofit.properties['Annual_Operating_Budget'];
+            if (budget>= 850000){
+              filteredData.push(nonprofit);
+            }
+          }
+        } else if (cb.value == '$350K - 100K'){
+          for (const nonprofit of nonprofits.features){
+            var budget = nonprofit.properties['Annual_Operating_Budget'];
+            if (budget< 850000 && budget >= 100000){
+              filteredData.push(nonprofit);
+            }
+          }
+        } else if (cb.value == '85K - 30K'){
+          for (const nonprofit of nonprofits.features){
+            var budget = nonprofit.properties['Annual_Operating_Budget'];
+            if (budget> 100000 && budget >= 30000){
+              filteredData.push(nonprofit);
+            }
+          }
+        } else {
+          for (const nonprofit of nonprofits.features){
+            var budget = nonprofit.properties['Annual_Operating_Budget'];
+            if (budget< 30000){
               filteredData.push(nonprofit);
             }
           }
